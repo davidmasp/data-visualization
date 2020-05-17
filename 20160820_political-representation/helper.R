f5701554e0090a75f3c8a081df128b6e8792dc13
David Mas-Ponte <david.mas@irbbarcelona.org>
2020-05-17 18:47:08 +0200

    major update

--- 20160820_political-representation/helper.R
+++ 20160820_political-representation/helper.R
@@ -1,17 +1,65 @@
+
+
+# imports -----------------------------------------------------------------
+
 library(ggplot2)
 library(ggrepel)
-
-el.df <- read.table("data/nationals.csv", header = TRUE,sep=";")
-el.df$election <- as.Date(el.df$election, origin="1899-12-30")
-total.votes = sum(el.df$votes)
-total.seats = sum(el.df$seats)
-el.df$votes.per <- el.df$votes / total.votes
-el.df$seats.per <- el.df$seats / total.seats
-el.df$party <- as.factor(el.df$party)
+library(dplyr)
+library(scales)
 
 
+# region helpers ----------------------------------------------------------
+
+# building the region
+tri.df <- data.frame(votes.per = c(0,1,1),
+                     seats.per = c(0,1,0) ) 
+
+# data processing ---------------------------------------------------------
+
+dat <- read.table("data/nationals.csv",
+                    header = TRUE,
+                    sep=";")
+
+dat$election <- as.Date(dat$election, origin="1899-12-30")
+total.votes = sum(dat$votes)
+total.seats = sum(dat$seats)
+dat$votes.per <- dat$votes / total.votes
+dat$seats.per <- dat$seats / total.seats
+dat$party <- as.factor(dat$party)
+
+dat$party_label = paste(dat$party,format(dat$election,"%Y"),sep = " - ")
+
+opt_choices = levels(dat$party) %>% as.list()
+names(opt_choices) = unlist(opt_choices)
+
+# colors ------------------------------------------------------------------
 
 
-
-
-
+colors <- c("Amaiur" = "#0198B3",
+               "Bildu" = "#BDD016",
+               "BNG" = "#6EC9FF",
+               "CC" = "#FFED03", 
+               "CDC" = "darkblue", 
+               "Compromis" = "orange", 
+               "Cs" = "#F17A36", 
+               "EB" = "grey", 
+               "ECP" = "blueviolet", 
+               "EnMarea" = "blue4", 
+               "EQUO"="green4", 
+               "ERC" = "orange", 
+               "Foro" = "royalblue4", 
+               "GeroaBai" = "#4dff4d", 
+               "IU" = "red2", 
+               "PA" = "palegreen2", 
+               "PACMA" = "palegreen4",
+               "PNV" = "#419653",
+               "PODEMOS" = "darkmagenta",
+               "PP" = "dodgerblue3",
+               "PRC" = "darkred",
+               "PSOE" = "red2",
+               "PxC" = "paleturquoise3",
+               "UPYD" = "violetred1",
+               "GeroaBai" = "seagreen1",
+               "CiU" = "#000099",
+               "DL" = "#0000b3",
+               "UP" = "darkmagenta")
--- 20160820_political-representation/ui.R
+++ 20160820_political-representation/ui.R
@@ -1,13 +1,16 @@
 shinyUI(pageWithSidebar(
   headerPanel('Stats - Spain General Election'),
   sidebarPanel(
-    selectInput('group', 'Party or Coalition', c("party","coalition")),
-    #checkboxGroupInput(),
+    checkboxGroupInput("selectedParty", 
+                       h3("Checkbox group"), 
+                       choices = opt_choices,
+                       selected = c("PP","PSOE")),
     sliderInput('filtervotes', '% votes cutoff', c(0.025,0.16),
-                 min = 0, max = 0.16, step = 0.005)
-    
+                 min = 0, max = 0.16, step = 0.005),
+    dateRangeInput("dates", h3("Date range"))
   ),
   mainPanel(
     plotOutput('plot1')
   )
 ))
+
