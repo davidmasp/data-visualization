bdde0bde369cbb240f4591f37095a72de794a543
David Mas-Ponte <david.mas@irbbarcelona.org>
2021-08-03 00:46:53 +0200

    :mark: Update the world record mark

---
+++ 20210801_worldRecord/readme.md
@@ -0,0 +1,18 @@
+# README
+
+![](drawing.png)
+
+## Comment for reddit
+
+[OC] After 26 years, Yulimar Rojas breaks the world record in women's triple jump
+
+The chart shows the Women's Triple Jump World record progression from 1980 to now. The
+record was broken today in the Tokyo's Olympics after 26 years by Yulimar Rojas from Venezuela.
+The record progression started to grow after the 80s and stopped
+drastically after the 90s with Inessa Kravets from Ukraine.
+Prior to 1980 there were also long periods of record stability but I am
+unsure if this was due to less standardized ways of measuring records or less
+competitiveness.
+
+* Source: https://en.wikipedia.org/wiki/Triple_jump_world_record_progression#Women
+* Tools Used: R and ggplot for the initial wrangling and plotting the data points, then Inkscape to add svg flags from openmoji
---
+++ 20210801_worldRecord/script.R
@@ -0,0 +1,68 @@
+
+
+# impots ------------------------------------------------------------------
+
+library(magrittr)
+library(ggplot2)
+library(readr)
+
+# data --------------------------------------------------------------------
+
+beforeIAAF = "01-01-1912"
+
+cols(
+  Time = col_double(),
+  Athlete = col_character(),
+  Nationality = col_character(),
+  Location = col_character(),
+  Date = col_date(format = "")
+) -> col_types
+
+dat = readr::read_csv("Sheet1.csv",col_types = col_types)
+
+# script ------------------------------------------------------------------
+
+ggplot(dat, aes(x = Date, y = Time)) +
+  geom_point() +
+  geom_line()
+
+
+
+# script2 -----------------------------------------------------------------
+
+pre_IAAF = "1990-01-01" %>% as.Date()
+
+col_types = cols(
+  Mark = col_character(),
+  Athlete = col_character(),
+  Date = col_date(format = ""),
+  Location = col_character()
+)
+dat = readr::read_csv("data/women_triple_jump.csv", col_types = col_types)
+
+
+dat$Mark_m = dat$Mark %>% stringr::str_extract("[:digit:]+\\.[:digit:]+") %>%
+  as.numeric()
+set.seed(42)
+min_date = "1980-01-01" %>% as.Date()
+dat %>%
+  dplyr::filter(Date > min_date ) %>%
+    ggplot( aes(x = Date, y = Mark_m)) +
+  annotate(geom = "rect",
+           xmin = min_date,
+           fill = "gray90",
+           xmax = pre_IAAF,
+           ymin = -Inf,
+           ymax = Inf) +
+    geom_point() +
+    geom_line() +
+    geom_vline(xintercept = pre_IAAF, linetype = "dashed") +
+    ggrepel::geom_text_repel(aes(label = Athlete)) +
+    theme_classic() +
+  theme(text = element_text(size = 10),
+        axis.ticks = element_blank()) +
+  scale_x_date(expand = expansion(mult = c(0,0), add = c(0,0))) +
+  scale_y_continuous(labels = scales::unit_format(accuracy = 1)) +
+  labs(y = "Distance", x = "Year")
+
+ggsave("plot.pdf", width = 8, height = 4)
